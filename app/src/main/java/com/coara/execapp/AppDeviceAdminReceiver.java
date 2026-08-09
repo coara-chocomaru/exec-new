7c2e5ff7cc2b5385f8625702732e9f79483749ae
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-09 19:40:01 +0900

    Delete app/src/main/java/com/coara/execapp directory

--- app/src/main/java/com/coara/execapp/AppDeviceAdminReceiver.java
+++
@@ -1,7 +0,0 @@
-package com.coara.execapp;
-import android.app.admin.DeviceAdminReceiver;
-import android.content.Context;
-import android.content.Intent;
-
-public class AppDeviceAdminReceiver extends DeviceAdminReceiver {
-}
