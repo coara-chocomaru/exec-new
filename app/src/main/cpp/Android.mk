4a69ee84ed770d266aeb339ccbcf740d3520159a
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-16 15:55:49 +0900

    Change LOCAL_CPPFLAGS to LOCAL_CFLAGS in Android.mk

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -1,7 +1,10 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
+
 LOCAL_MODULE    := pocjni
 LOCAL_SRC_FILES := poc_jni.c
 LOCAL_LDLIBS    := -llog
-LOCAL_CPPFLAGS  := -std=c99
+LOCAL_CFLAGS    := -std=c99
+
 include $(BUILD_SHARED_LIBRARY)
