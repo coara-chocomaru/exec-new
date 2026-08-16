f1ad64597aa36e62ddbcd43c5c95b688c707a39c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-16 15:43:20 +0900

    Add Android.mk for JNI module configuration

---
+++ app/src/main/cpp/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := pocjni
+LOCAL_SRC_FILES := poc_jni.c
+LOCAL_LDLIBS    := -llog
+LOCAL_CPPFLAGS  := -std=c99
+include $(BUILD_SHARED_LIBRARY)
