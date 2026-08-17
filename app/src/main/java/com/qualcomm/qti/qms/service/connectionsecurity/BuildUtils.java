bf62a08afa98ab1aa48d3bbb4966b8e8437ff614
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-18 08:22:11 +0900

    Add BuildUtils class for native build operations

---
+++ app/src/main/java/com/qualcomm/qti/qms/service/connectionsecurity/BuildUtils.java
@@ -0,0 +1,7 @@
+package com.qualcomm.qti.qms.service.connectionsecurity;
+
+public class BuildUtils {
+    public native void nativeInit();
+    public native void nativeDestroy();
+    public native String nativeGetBuildFlavor();
+}
