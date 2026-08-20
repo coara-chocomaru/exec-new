8c1fc3b7863bbfaf5ab9725d6d74ab653a416c96
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-20 18:08:00 +0900

    Rename testNativeCommand to runCommand and remove unused function
    Renamed the testNativeCommand function to runCommand and removed the triggerStackOverflow function.

--- app/src/main/cpp/wigig_poc.c
+++ app/src/main/cpp/wigig_poc.c
@@ -2,12 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <android/log.h>
-
-#define LOG_TAG "WigigPoC"
 
 JNIEXPORT jstring JNICALL
-Java_com_wigig_poc_MainActivity_testNativeCommand(JNIEnv *env, jobject thiz, jstring cmd) {
+Java_com_wigig_poc_MainActivity_runCommand(JNIEnv *env, jobject thiz, jstring cmd) {
     const char *cmd_str = (*env)->GetStringUTFChars(env, cmd, NULL);
     char result[4096] = {0};
     FILE *fp = popen(cmd_str, "r");
@@ -19,14 +16,4 @@ Java_com_wigig_poc_MainActivity_testNativeCommand(JNIEnv *env, jobject thiz, jst
     }
     (*env)->ReleaseStringUTFChars(env, cmd, cmd_str);
     return (*env)->NewStringUTF(env, result);
-}
-
-JNIEXPORT void JNICALL
-Java_com_wigig_poc_MainActivity_triggerStackOverflow(JNIEnv *env, jobject thiz, jstring longParam) {
-    const char *param = (*env)->GetStringUTFChars(env, longParam, NULL);
-    char buffer[128];
-    // 意図的なバッファオーバーフロー（サイズチェックなし）
-    strcpy(buffer, param);
-    __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "Copied %s (len=%zu)", buffer, strlen(buffer));
-    (*env)->ReleaseStringUTFChars(env, longParam, param);
 }
