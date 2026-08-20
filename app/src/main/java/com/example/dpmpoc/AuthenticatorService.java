0843c434f92829c7c7fbd9be22ef4c6de8926398
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-21 07:59:06 +0900

    Add AuthenticatorService class for authentication

---
+++ app/src/main/java/com/example/dpmpoc/AuthenticatorService.java
@@ -0,0 +1,20 @@
+package com.example.dpmpoc;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AuthenticatorService extends Service {
+    private MyAuthenticator authenticator;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        authenticator = new MyAuthenticator(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return authenticator.getIBinder();
+    }
+}
