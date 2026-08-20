a018e6661351d825148b85e0a76438652c7e0b22
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-21 07:27:17 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/dpmpoc/MainActivity.java
+++ app/src/main/java/com/example/dpmpoc/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Parcel;
 import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
