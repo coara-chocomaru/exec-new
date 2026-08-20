36634500c95f64f4f0d4180bf3ad95d174fb46e8
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-20 17:40:50 +0900

    Rename module and source file in Android.mk

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -1,10 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
-
-LOCAL_MODULE    := pocjni
-LOCAL_SRC_FILES := poc_jni.c
-LOCAL_LDLIBS    := -llog
-LOCAL_CFLAGS    := -std=c99
-
+LOCAL_MODULE := wigig_poc
+LOCAL_SRC_FILES := wigig_poc.c
+LOCAL_LDLIBS := -llog
 include $(BUILD_SHARED_LIBRARY)
