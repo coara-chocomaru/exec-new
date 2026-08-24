ed9e63f398bf86b80edf38f61428e7824f5a1c85
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-25 04:58:40 +0900

    Update Android.mk

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := pocjni
-LOCAL_SRC_FILES := poc_jni.c
-LOCAL_LDLIBS    := -llog
-LOCAL_CFLAGS    := -std=c99
+LOCAL_MODULE := dumppoc
+LOCAL_SRC_FILES := dump.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_LDFLAGS := -Wl,--exclude-libs,libgcc.a -Wl,--exclude-libs,libatomic.a
+LOCAL_LDLIBS := -llog
+LOCAL_CFLAGS := -O2 -fPIC
 
 include $(BUILD_SHARED_LIBRARY)
