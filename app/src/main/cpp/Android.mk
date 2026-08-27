0a82fd2d9f013e34bbd8d65839f8c991aadf7bea
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-28 05:12:56 +0900

    Update Android.mk

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -6,4 +6,9 @@ LOCAL_MODULE := exploit
 LOCAL_SRC_FILES := exploit.c
 LOCAL_LDLIBS := -llog
 LOCAL_CFLAGS := -Wall -O2 -fPIC
+LOCAL_LDFLAGS := -Wl,-soname,exploit.so \
+                 -Wl,--no-undefined \
+                 -Wl,-z,noexecstack \
+                 -Wl,-z,relro \
+                 -Wl,-z,now
 include $(BUILD_SHARED_LIBRARY)
