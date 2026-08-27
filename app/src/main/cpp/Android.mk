56b99c36a0444b9ef9a4e9d9a5b0d817daf42a8f
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-28 04:34:06 +0900

    Update Android.mk with new compiler flags and mode

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -5,5 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := exploit
 LOCAL_SRC_FILES := exploit.c
 LOCAL_LDLIBS := -llog
-LOCAL_CFLAGS := -std=c99
+LOCAL_CFLAGS := -Wall -O2 -fPIC
+LOCAL_ARM_MODE := arm
 include $(BUILD_SHARED_LIBRARY)
