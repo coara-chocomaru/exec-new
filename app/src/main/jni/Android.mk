e0eafd71c5a908b9045d01e8196b26c666061273
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-29 06:49:35 +0900

    Rename module and source file in Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := native-inspector
-LOCAL_SRC_FILES := native-lib.c
+LOCAL_MODULE := libpoc
+LOCAL_SRC_FILES := libpoc.c
 LOCAL_LDLIBS := -llog
 LOCAL_CFLAGS := -Wall -O2 -fPIC
 LOCAL_LDFLAGS := -Wl,-soname,libnative-inspector.so \
