7cea24460c8a5a2efca9d7736cfb145cb387207b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-29 06:50:54 +0900

    Simplify linker flags in Android.mk
    Removed unnecessary linker flags from Android.mk.

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -6,9 +6,4 @@ LOCAL_MODULE := libpoc
 LOCAL_SRC_FILES := libpoc.c
 LOCAL_LDLIBS := -llog
 LOCAL_CFLAGS := -Wall -O2 -fPIC
-LOCAL_LDFLAGS := -Wl,-soname,libnative-inspector.so \
-                 -Wl,--no-undefined \
-                 -Wl,-z,noexecstack \
-                 -Wl,-z,relro \
-                 -Wl,-z,now
 include $(BUILD_SHARED_LIBRARY)
