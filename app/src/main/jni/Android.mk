fd2e938f853ce213f45648a5a3a503177d5f6a0c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-29 07:03:58 +0900

    Rename module from libpoc to poc and update flags

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libpoc
+LOCAL_MODULE := poc
 LOCAL_SRC_FILES := libpoc.c
 LOCAL_LDLIBS := -llog
-LOCAL_CFLAGS := -Wall -O2 -fPIC
+LOCAL_CFLAGS := -Wall -Wextra -O2 -D_GNU_SOURCE
 include $(BUILD_SHARED_LIBRARY)
